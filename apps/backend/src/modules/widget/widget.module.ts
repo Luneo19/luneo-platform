60457d210260033efde68766c60c63327a2b9d56
Luneo19 <service.luneo@gmail.com>
2026-01-04 23:24:13 +0100

    fix: Import ApiKeysModule in WidgetModule for ApiKeyGuard

--- apps/backend/src/modules/widget/widget.module.ts
+++ apps/backend/src/modules/widget/widget.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { WidgetController } from './widget.controller';
 import { WidgetService } from './widget.service';
 import { PrismaModule } from '@/libs/prisma/prisma.module';
+import { ApiKeysModule } from '../public-api/api-keys/api-keys.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ApiKeysModule],
   controllers: [WidgetController],
   providers: [WidgetService],
   exports: [WidgetService],
 })
 export class WidgetModule {}
+
 
 
