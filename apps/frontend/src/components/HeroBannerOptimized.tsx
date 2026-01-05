fef33ebb1799c092155a750eb4d63efb8ffd0660
Luneo19 <service.luneo@gmail.com>
2026-01-05 09:49:17 +0100

    fix: ajouter import Image depuis next/image dans HeroBannerOptimized

--- apps/frontend/src/components/HeroBannerOptimized.tsx
+++ apps/frontend/src/components/HeroBannerOptimized.tsx
@@ -14,6 +14,7 @@
  */
 
 import React, { useMemo } from 'react';
+import Image from 'next/image';
 import styles from './HeroBannerOptimized.module.css';
 
 // Import direct pour performance (les composants sont déjà légers avec SVG/CSS)
